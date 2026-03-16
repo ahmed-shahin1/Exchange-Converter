10a87efe096d873bc859488c37a4d1daa623fef8
Ahmed Shahin <ahmed.nidalshahin@gmail.com>
2026-03-16 16:13:27 +0400

    Initial app

---
+++ Exchange Convert/Exchange Convert/Exchange_ConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exchange_ConvertApp.swift
+//  Exchange Convert
+//
+//  Created by Osama Nidal on 16/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Exchange_ConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

d6d0c44bec8a28b8ce3ae6bd85b455bf749a751b
Ahmed Shahin <ahmed.nidalshahin@gmail.com>
2026-03-23 06:33:52 +0400

    Added Color background and rectangle for the content

--- Exchange Convert/Exchange Convert/ContentView.swift
+++ Exchange Convert/Exchange Convert/ContentView.swift
@@ -1,17 +1,27 @@
 
 import SwiftUI
 
+let backgroundGradient = LinearGradient(colors: [Color.orange,Color.red], startPoint: .top, endPoint: .bottom)
+
 struct ContentView: View {
     var body: some View {
-        
-        NavigationView{
-            VStack {
-                
+        NavigationStack{
+            ZStack {
+               backgroundGradient
+                    .ignoresSafeArea()
+                VStack {
+                    Rectangle()
+                        .fill(Color.white)
+                        .cornerRadius(30)
+                        .padding(.top)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .ignoresSafeArea(edges: .bottom)
+                }
+                .navigationTitle("Welcome")
+                .font(.title2)
+                .toolbarColorScheme(.dark, for: .navigationBar)
             }
-            .navigationTitle("Welcome,")
-            .padding()
         }
-        
     }
 }
 

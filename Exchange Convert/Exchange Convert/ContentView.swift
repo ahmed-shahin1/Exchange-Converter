1f9d7027e71e71cc34d7763454a23b9257168232
Ahmed Shahin <ahmed.nidalshahin@gmail.com>
2026-03-24 06:42:01 +0400

    Overlay text and alignment navigation title

--- Exchange Convert/Exchange Convert/ContentView.swift
+++ Exchange Convert/Exchange Convert/ContentView.swift
@@ -9,21 +9,34 @@ struct ContentView: View {
             ZStack {
                backgroundGradient
                     .ignoresSafeArea()
-                VStack {
+                VStack (alignment: .leading){
                     Rectangle()
                         .fill(Color.white)
                         .cornerRadius(30)
                         .padding(.top)
                         .frame(maxWidth: .infinity, maxHeight: .infinity)
                         .ignoresSafeArea(edges: .bottom)
+                        .overlay(starOverlay)
                 }
                 .navigationTitle("Welcome")
-                .font(.title2)
+                .font(.title)
                 .toolbarColorScheme(.dark, for: .navigationBar)
             }
         }
     }
 }
+
+private var starOverlay: some View {
+    VStack(alignment: .leading){
+        Text("Exchange rate")
+            .frame(maxWidth: .infinity,alignment: .leading)
+            .font(.title2)
+            .bold()
+        Spacer()
+    }
+    .padding(.top, 60)
+    .padding(.leading, 20)
+   }
 
 #Preview {
     ContentView()

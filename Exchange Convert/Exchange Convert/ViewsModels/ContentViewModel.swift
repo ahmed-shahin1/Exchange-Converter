71a0f6d896cc5d5c2cd9807438f5acdaea0f288f
Ahmed Shahin <ahmed.nidalshahin@gmail.com>
2026-04-02 05:52:53 +0400

    added view to View folder

---
+++ Exchange Convert/Exchange Convert/ViewsModels/ContentViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ContentViewModel.swift
+//  Exchange Convert
+//
+//  Created by Osama Nidal on 02/04/2026.
+//
+
+import SwiftUI
+
+struct ContentViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentViewModel()
+}

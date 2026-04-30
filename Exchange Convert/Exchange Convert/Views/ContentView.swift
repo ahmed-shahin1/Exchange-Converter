8d7fb56646b79eb478650bdc509f1867327e4a8c
Ahmed Shahin <ahmed.nidalshahin@gmail.com>
2026-04-30 17:11:14 +0400

    combine the star overley with the view and add buttons

--- Exchange Convert/Exchange Convert/Views/ContentView.swift
+++ Exchange Convert/Exchange Convert/Views/ContentView.swift
@@ -1,9 +1,11 @@
 
 import SwiftUI
 
+
 let backgroundGradient = LinearGradient(colors: [Color.orange,Color.red], startPoint: .leading, endPoint: .trailing)
 
 struct ContentView: View {
+    @StateObject var vm = RatesModel()
     var body: some View {
         NavigationStack{
             ZStack {
@@ -23,26 +25,31 @@ struct ContentView: View {
             }
         }
     }
+    private var starOverlay: some View {
+        
+        VStack(alignment: .leading){
+            Text("Exchange rate")
+                .frame(maxWidth: .infinity,alignment: .leading)
+                .font(.title2)
+                .bold()
+            
+            HStack(){
+                Buttons()
+                Image(systemName: "arrow.right")
+                    .font(.system(size: 20))
+                Buttons()
+            }
+        
+            Spacer()
+                
+        }
+        .padding(.top, 60)
+        .padding(.leading, 20)
+        .padding(.trailing,20)
+       }
 }
 
-private var starOverlay: some View {
-    VStack(alignment: .leading){
-        Text("Exchange rate")
-            .frame(maxWidth: .infinity,alignment: .leading)
-            .font(.title2)
-            .bold()
-        HStack(){
-            menus()
-            Image(systemName: "arrow.right")
-                .font(.system(size: 20))
-            menus()
-        }
-        Spacer()
-    }
-    .padding(.top, 60)
-    .padding(.leading, 20)
-    .padding(.trailing,20)
-   }
+
 
 #Preview {
     ContentView()

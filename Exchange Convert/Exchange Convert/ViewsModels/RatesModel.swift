e5c096ce467105028b079a1829f3937d7577d907
Ahmed Shahin <ahmed.nidalshahin@gmail.com>
2026-04-30 17:09:34 +0400

    Connected to API

--- Exchange Convert/Exchange Convert/ViewsModels/RatesModel.swift
+++ Exchange Convert/Exchange Convert/ViewsModels/RatesModel.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+import Combine
+
+
+
+final class RatesModel: ObservableObject{
+    let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String ?? ""
+    @Published var rates: Rate? = nil
+    func rateShow() async throws -> Rate{
+        let endpoint = "https://openexchangerates.org/api/latest.json?app_id=\(apiKey)"
+        guard let url = URL(string: endpoint) else{
+            throw GHError.invalidUrl
+        }
+        
+        let (data,response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+            throw GHError.invalidResponse
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            return try decoder.decode(Rate.self,from: data)
+        }catch{
+            throw GHError.invalidData
+        }
+    }
+    
+    func loadRates() async {
+        do {
+            let result = try await rateShow() // go fetch
+            self.rates = result                // save it!
+        } catch {
+            print("Went something wrong......")
+        }
+    }
+}
+
+
+enum GHError: Error{
+    case invalidUrl
+    case invalidResponse
+    case invalidData
+    case invalidLoad
+}

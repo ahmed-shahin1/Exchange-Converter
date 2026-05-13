2bbe1f0cb7b735317c9858fde0e16feadb788cb8
Ahmed Shahin <ahmed.nidalshahin@gmail.com>
2026-05-13 18:07:44 +0400

    list of currency

--- Exchange Convert/Exchange Convert/Models/ChooseCurrency.swift
+++ Exchange Convert/Exchange Convert/Models/ChooseCurrency.swift
@@ -1,1 +1,156 @@
-
+struct CurrencyData {
+    
+    static let currencyToCountry: [String:String] = [
+        "AED": "AE", // UAE
+        "AFN": "AF", // Afghanistan
+        "ALL": "AL", // Albania
+        "AMD": "AM", // Armenia
+        "ANG": "NL", // Netherlands Antilles
+        "AOA": "AO", // Angola
+        "ARS": "AR", // Argentina
+        "AUD": "AU", // Australia
+        "AWG": "AW", // Aruba
+        "AZN": "AZ", // Azerbaijan
+        "BAM": "BA", // Bosnia
+        "BBD": "BB", // Barbados
+        "BDT": "BD", // Bangladesh
+        "BGN": "BG", // Bulgaria
+        "BHD": "BH", // Bahrain
+        "BIF": "BI", // Burundi
+        "BMD": "BM", // Bermuda
+        "BND": "BN", // Brunei
+        "BOB": "BO", // Bolivia
+        "BRL": "BR", // Brazil
+        "BSD": "BS", // Bahamas
+        "BTN": "BT", // Bhutan
+        "BWP": "BW", // Botswana
+        "BYN": "BY", // Belarus
+        "BZD": "BZ", // Belize
+        "CAD": "CA", // Canada
+        "CDF": "CD", // Congo
+        "CHF": "CH", // Switzerland
+        "CLP": "CL", // Chile
+        "CNY": "CN", // China
+        "COP": "CO", // Colombia
+        "CRC": "CR", // Costa Rica
+        "CUP": "CU", // Cuba
+        "CVE": "CV", // Cape Verde
+        "CZK": "CZ", // Czech Republic
+        "DJF": "DJ", // Djibouti
+        "DKK": "DK", // Denmark
+        "DOP": "DO", // Dominican Republic
+        "DZD": "DZ", // Algeria
+        "EGP": "EG", // Egypt
+        "ERN": "ER", // Eritrea
+        "ETB": "ET", // Ethiopia
+        "EUR": "EU", // Europe
+        "FJD": "FJ", // Fiji
+        "GBP": "GB", // UK
+        "GEL": "GE", // Georgia
+        "GHS": "GH", // Ghana
+        "GMD": "GM", // Gambia
+        "GNF": "GN", // Guinea
+        "GTQ": "GT", // Guatemala
+        "GYD": "GY", // Guyana
+        "HKD": "HK", // Hong Kong
+        "HNL": "HN", // Honduras
+        "HRK": "HR", // Croatia
+        "HTG": "HT", // Haiti
+        "HUF": "HU", // Hungary
+        "IDR": "ID", // Indonesia
+        "INR": "IN", // India
+        "IQD": "IQ", // Iraq
+        "IRR": "IR", // Iran
+        "ISK": "IS", // Iceland
+        "JMD": "JM", // Jamaica
+        "JOD": "JO", // Jordan
+        "JPY": "JP", // Japan
+        "KES": "KE", // Kenya
+        "KGS": "KG", // Kyrgyzstan
+        "KHR": "KH", // Cambodia
+        "KMF": "KM", // Comoros
+        "KPW": "KP", // North Korea
+        "KRW": "KR", // South Korea
+        "KWD": "KW", // Kuwait
+        "KYD": "KY", // Cayman Islands
+        "KZT": "KZ", // Kazakhstan
+        "LAK": "LA", // Laos
+        "LBP": "LB", // Lebanon
+        "LKR": "LK", // Sri Lanka
+        "LRD": "LR", // Liberia
+        "LSL": "LS", // Lesotho
+        "LYD": "LY", // Libya
+        "MAD": "MA", // Morocco
+        "MDL": "MD", // Moldova
+        "MGA": "MG", // Madagascar
+        "MKD": "MK", // Macedonia
+        "MMK": "MM", // Myanmar
+        "MNT": "MN", // Mongolia
+        "MOP": "MO", // Macao
+        "MRU": "MR", // Mauritania
+        "MUR": "MU", // Mauritius
+        "MVR": "MV", // Maldives
+        "MWK": "MW", // Malawi
+        "MXN": "MX", // Mexico
+        "MYR": "MY", // Malaysia
+        "MZN": "MZ", // Mozambique
+        "NAD": "NA", // Namibia
+        "NGN": "NG", // Nigeria
+        "NIO": "NI", // Nicaragua
+        "NOK": "NO", // Norway
+        "NPR": "NP", // Nepal
+        "NZD": "NZ", // New Zealand
+        "OMR": "OM", // Oman
+        "PAB": "PA", // Panama
+        "PEN": "PE", // Peru
+        "PGK": "PG", // Papua New Guinea
+        "PHP": "PH", // Philippines
+        "PKR": "PK", // Pakistan
+        "PLN": "PL", // Poland
+        "PYG": "PY", // Paraguay
+        "QAR": "QA", // Qatar
+        "RON": "RO", // Romania
+        "RSD": "RS", // Serbia
+        "RUB": "RU", // Russia
+        "RWF": "RW", // Rwanda
+        "SAR": "SA", // Saudi Arabia
+        "SBD": "SB", // Solomon Islands
+        "SCR": "SC", // Seychelles
+        "SDG": "SD", // Sudan
+        "SEK": "SE", // Sweden
+        "SGD": "SG", // Singapore
+        "SLL": "SL", // Sierra Leone
+        "SOS": "SO", // Somalia
+        "SRD": "SR", // Suriname
+        "STN": "ST", // São Tomé
+        "SVC": "SV", // El Salvador
+        "SYP": "SY", // Syria
+        "SZL": "SZ", // Swaziland
+        "THB": "TH", // Thailand
+        "TJS": "TJ", // Tajikistan
+        "TMT": "TM", // Turkmenistan
+        "TND": "TN", // Tunisia
+        "TOP": "TO", // Tonga
+        "TRY": "TR", // Turkey
+        "TTD": "TT", // Trinidad
+        "TWD": "TW", // Taiwan
+        "TZS": "TZ", // Tanzania
+        "UAH": "UA", // Ukraine
+        "UGX": "UG", // Uganda
+        "USD": "US", // USA
+        "UYU": "UY", // Uruguay
+        "UZS": "UZ", // Uzbekistan
+        "VES": "VE", // Venezuela
+        "VND": "VN", // Vietnam
+        "VUV": "VU", // Vanuatu
+        "WST": "WS", // Samoa
+        "XAF": "CM", // Central Africa
+        "XOF": "SN", // West Africa
+        "YER": "YE", // Yemen
+        "ZAR": "ZA", // South Africa
+        "ZMW": "ZM", // Zambia
+        "ZWL": "ZW"  // Zimbabwe
+    ]
+    
+    
+}
